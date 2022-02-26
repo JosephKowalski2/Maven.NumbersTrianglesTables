593864947da8170aac22a64d71db631f743e65e6
Joseph Kowalski <jkjr25@gmail.com>
2022-02-26 13:43:39 -0500

    Finished getEven and getOdd

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,12 +3,29 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+
+        String evenNumbers = "";
+
+        for (int i = start; i < stop; i++){
+            if (i % 2 == 0){
+                evenNumbers += Integer.toString(i);
+            }
+        }
+        return evenNumbers;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+
+        String oddNumbers = "";
+
+        for (int i = start; i < stop; i++){
+            if (i % 2 != 0){
+                oddNumbers += Integer.toString(i);
+            }
+        }
+        return oddNumbers;
+
     }
 
 
