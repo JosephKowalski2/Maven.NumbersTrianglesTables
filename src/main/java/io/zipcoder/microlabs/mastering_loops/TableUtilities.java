8cc1acd785796febb6cc5a85a591353a6d79e3cc
Joseph Kowalski <jkjr25@gmail.com>
2022-02-26 22:26:44 -0500

    Completed table utilities. Can't get format correctly for lines

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -1,15 +1,65 @@
 package io.zipcoder.microlabs.mastering_loops;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        StringBuilder smallMulti = new StringBuilder();
+
+        for (int i = 1; i <= 5; i++){
+            for (int l = 1; l <= 5; l++){
+                if ((i * l) < 10) {
+                    smallMulti.append(" ");
+                }
+                if (l == 1){
+                    smallMulti.append(" ").append(i * l);
+                    }
+                else {
+                    smallMulti.append(" | ").append(i * l);
+                }
+            }
+            smallMulti.append("\n");
+        }
+        return smallMulti.toString();
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        StringBuilder smallMulti = new StringBuilder();
+
+        for (int i = 1; i <= 10; i++){
+            for (int l = 1; l <= 10; l++){
+                if ((i * l) < 10) {
+                    smallMulti.append(" ");
+                }
+                if (l == 1){
+                    smallMulti.append(" ").append(i * l);
+                }
+                else {
+                    smallMulti.append(" | ").append(i * l);
+                }
+            }
+            smallMulti.append("\n");
+        }
+        return smallMulti.toString();
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder smallMulti = new StringBuilder();
+
+        for (int i = 1; i <= tableSize; i++){
+            for (int l = 1; l <= tableSize; l++){
+                if ((i * l) < 10) {
+                    smallMulti.append(" ");
+                }
+                if (l == 1){
+                    smallMulti.append(" ").append(i * l);
+                }
+                else {
+                    smallMulti.append(" | ").append(i * l);
+                }
+            }
+            smallMulti.append("\n");
+        }
+        return smallMulti.toString();
     }
 }
