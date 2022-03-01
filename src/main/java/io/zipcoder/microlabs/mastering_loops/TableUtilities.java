9161d2d8c08702f96bbc87c0d62cc23f62ea79f0
Joseph Kowalski <jkjr25@gmail.com>
2022-03-01 15:44:25 -0500

    Completed

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -4,62 +4,22 @@ import org.w3c.dom.ls.LSOutput;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        StringBuilder smallMulti = new StringBuilder();
-
-        for (int i = 1; i <= 5; i++){
-            for (int l = 1; l <= 5; l++){
-                if ((i * l) < 10) {
-                    smallMulti.append(" ");
-                }
-                if (l == 1){
-                    smallMulti.append(" ").append(i * l);
-                    }
-                else {
-                    smallMulti.append(" | ").append(i * l);
-                }
-            }
-            smallMulti.append("\n");
-        }
-        return smallMulti.toString();
+        return TableUtilities.getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        StringBuilder largeMulti = new StringBuilder();
-
-        for (int i = 1; i <= 10; i++){
-            for (int l = 1; l <= 10; l++){
-                if ((i * l) < 10) {
-                    largeMulti.append(" ");
-                }
-                if (l == 1){
-                    largeMulti.append(" ").append(i * l);
-                }
-                else {
-                    largeMulti.append(" | ").append(i * l);
-                }
-            }
-            largeMulti.append("\n");
-        }
-        return largeMulti.toString();
+            return TableUtilities.getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        StringBuilder tableMulti = new StringBuilder();
+        String tableMulti = "";
 
         for (int i = 1; i <= tableSize; i++){
-            for (int l = 1; l <= tableSize; l++){
-                if ((i * l) < 10) {
-                    tableMulti.append(" ");
-                }
-                if (l == 1){
-                    tableMulti.append(" ").append(i * l);
-                }
-                else {
-                    tableMulti.append(" | ").append(i * l);
-                }
+            for (int t = 1; t <= tableSize; t++){
+                tableMulti += String.format("%3d |", i * t);
             }
-            tableMulti.append("\n");
+            tableMulti += "\n";
         }
-        return tableMulti.toString();
+        return tableMulti;
     }
 }
